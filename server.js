16135dc0d33b5556be960ca2dfe7eb18923a1ad5
bdkinzer <bdkinzer@gmail.com>
2018-10-26 15:32:30 -0500

    Moved some code from server.js to its own file
    Cleaned up server.js by moving routes into app/routes/api.js

--- server.js
+++ server.js
@@ -23,27 +23,7 @@ mongoose.connect('mongodb://localhost:27017/tutorial', { useNewUrlParser: true }
 });
 mongoose.set('useCreateIndex', true);
 
-// adds user to database
-app.post('/users', function(req, res) {
-  var user = new User();
-  user.email = req.body.username;
-  user.password = req.body.password;
-  user.name = req.body.name;
-  
-  // makes sure all necessary fields filled in
-  if (req.body.email == null || req.body.email == '' || req.body.password == null || req.body.password == '' || req.body.name == null || req.body.name == '') {
-    res.send("Ensure, email, password, and name were provided.");
-  } else {
-    user.save(function(err) {
-      if (err) {
-        res.send('Email already exists.');
-      } else {
-        res.send('User created');
-      }
-    });
-  }
-});
-
+// server port
 app.listen(port, function() {
   console.log('Server is running on port: ' + port);
 });
