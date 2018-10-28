6e3ff098737268fc26901845064946d4b9eab9b7
bdkinzer <bdkinzer@gmail.com>
2018-10-28 12:49:21 -0500

    Added password confirmation
    There is now a second password field on the create user page. Passwords must match before the user can be created.

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -24,4 +24,40 @@ angular.module('userControllers', ['userServices'])
       }
     });
   };
+})
+
+// checks whether two passwords match on registration
+.directive('match', function() {
+  return {
+    restrict: 'A',
+    controller: function($scope) {
+
+      $scope.confirmed = false;
+
+      $scope.doConfirm = function(values) {
+        values.forEach(function(ele) {
+
+          if ($scope.confirm == ele) {
+            $scope.confirmed = true;
+          } else {
+            $scope.confirmed = false;
+          }
+        });
+      }
+    },
+
+    link: function(scope, element, attrs) {
+      // parse data from first password field whenever something is typed and asks if it matches other field
+      attrs.$observe('match', function() {
+        scope.matches = JSON.parse(attrs.match);
+        scope.doConfirm(scope.matches);
+      });
+
+      // parse data from second password field whenever something is typed and asks if it matches other field
+      scope.$watch('confirm', function() {
+        scope.matches = JSON.parse(attrs.match);
+        scope.doConfirm(scope.matches);
+      });
+    }
+  };
 });
