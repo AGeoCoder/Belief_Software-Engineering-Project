0a7291cf748f3dbef75c22cdafc8acae207f26d9
Mingxi Home Windows <mxu88@wisc.edu>
2018-11-11 19:08:45 -0500

    merged version

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'primaryController', 'authServices','articleController','tabbarController'])
+var app = angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'primaryController', 'authServices','articleController','tabbarController',"tabbarController"])
 
 .config(function($httpProvider) {
   $httpProvider.interceptors.push('AuthInterceptors');
