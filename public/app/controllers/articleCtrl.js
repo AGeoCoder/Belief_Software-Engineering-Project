1a5d23db974f0faefae8f131784da7d4e205ae7a
jinzi39 <jzhangandover@gmail.com>
2018-11-14 10:46:29 -0600

    Implement related articles

--- public/app/controllers/articleCtrl.js
+++ public/app/controllers/articleCtrl.js
@@ -7,7 +7,14 @@ angular.module('articleController', [])
   if (app.articleID == "Intimate_Partner_Sexual_Violence") app.hide = true;
   //var path = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + {{article.articleID}}
   app.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.txt';
-
+  if (app.articleID == "Intimate_Partner_Sexual_Violence") {
+    app.relatedURL = '/article/What_is_sexual_assault'
+    app.relatedTitle = 'What is sexual assault'
+  }
+  if (app.articleID == "What_is_sexual_assault") {
+    app.relatedURL = '/article/Intimate_Partner_Sexual_Violence'
+    app.relatedTitle = 'Intimate Partner Sexual Violence'
+  }
   app.show = function(createData, confirmed) {
     app.hide = false;
   };
