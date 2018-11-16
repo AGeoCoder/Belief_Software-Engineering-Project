d084796cda49b804a6bd26c3b4b69a64ebacbb41
jinzi39 <jzhangandover@gmail.com>
2018-11-15 22:00:50 -0600

    Add tests for trusted.js

--- public/app/controllers/trusted.js
+++ public/app/controllers/trusted.js
@@ -1,5 +1,11 @@
-app.filter('trusted', ['$sce', function ($sce) {
+app.filter('trusted', function ($sce) {
     return function(url) {
         return $sce.trustAsResourceUrl(url);
     };
-}]);
+});
+
+// app.filter('trusted', ['$sce', function ($sce) {
+//     return function(url) {
+//         return $sce.trustAsResourceUrl(url);
+//     };
+// }]);
