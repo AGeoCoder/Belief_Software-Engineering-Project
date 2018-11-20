2729d17edde26312ad22f97ea440e66b767bb8f4
bdkinzer <bdkinzer@gmail.com>
2018-11-19 18:20:54 -0600

    Comments on history page
    Comments can be made on posts from the history page and seen from community as well as history

--- public/app/controllers/historyCtrl.js
+++ public/app/controllers/historyCtrl.js
@@ -1,7 +1,8 @@
-angular.module('historyController', ['postServices'])
+angular.module('historyController', ['postServices', 'authServices'])
 
 .controller('historyCtrl', function($http, $location, $scope, Post, Auth) {
   var app = this;
+  this.newComment = [];
 
   this.getPosts = function() {
     Post.myPosts().then(function(data) {
@@ -10,6 +11,41 @@ angular.module('historyController', ['postServices'])
       } else {
         app.errorMess = data.data.message;
       }
+    });
+  };
+
+  this.draftComment = function(id) {
+    this.newComment = [];
+    this.newComment.push(id);
+  };
+
+  this.cancelComment = function() {
+    this.newComment = [];
+  };
+
+  this.createComment = function(commentData) {
+    app.commentError = false;
+
+    if (!Auth.isLoggedIn()) {
+      app.commentError = 'You must be logged in to create a comment';
+      return;
+    }
+
+    Auth.getUser().then(function(data) {
+      app.commentData.commentor = data.data.name;
+      app.commentData.id = app.newComment[0];
+
+      Post.commentCreate(app.commentData).then(function(data) {
+        if (data.data.success) {
+          // show success Message
+          //app.successMsg = data.data.message;
+          app.cancelComment();
+          app.getPosts();
+        } else {
+          // show error message
+          app.commentError = data.data.message;
+        }
+      });
     });
   };
 
