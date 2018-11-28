69832f1fa71c26d4c113f75e55c73be2c3e237ee
jinzi39 <jzhangandover@gmail.com>
2018-11-28 17:04:49 -0600

    Add more articles

--- public/app/controllers/articleCtrl.js
+++ public/app/controllers/articleCtrl.js
@@ -5,7 +5,7 @@ angular.module('articleController', [])
   $scope.hide = false;
   $scope.articleID = $routeParams.articleID;
   if ($scope.articleID == "Intimate_Partner_Sexual_Violence") app.hide = true;
-    $scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.txt';
+    $scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.html';
     if ($scope.articleID == "Intimate_Partner_Sexual_Violence") {
       $scope.relatedURL = '/article/What_is_sexual_assault'
       $scope.relatedTitle = 'What is sexual assault'
@@ -15,7 +15,42 @@ angular.module('articleController', [])
       $scope.relatedTitle = 'Intimate Partner Sexual Violence'
     }
     if ($scope.articleID == "Depression") {
-      $scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.rtf';
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.html';
+      $scope.relatedURL = '/article/Flashbacks'
+      $scope.relatedTitle = 'Flashbacks'
+    }
+    if ($scope.articleID == "Flashbacks") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.html';
+      $scope.relatedURL = '/article/Drepression'
+      $scope.relatedTitle = 'Depression'
+    }
+    if ($scope.articleID == "PTSD") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.html';
+      $scope.relatedURL = '/article/Drepression'
+      $scope.relatedTitle = 'Depression'
+    }
+    if ($scope.articleID == "Consent") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.rtf';
+      $scope.relatedURL = '/article/Legal'
+      $scope.relatedTitle = 'Legal Role of Consent'
+    }
+    if ($scope.articleID == "Legal") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.rtf';
+      $scope.relatedURL = '/article/Consent'
+      $scope.relatedTitle = 'What Consent Looks Like'
+    }
+    if ($scope.articleID == "Wisconsin") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.rtf';
+      $scope.relatedURL = '/article/Drepression'
+      $scope.relatedTitle = 'Intimate Partner Sexual Violence'
+    }
+    if ($scope.articleID == "Illinois") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.rtf';
+      $scope.relatedURL = '/article/Drepression'
+      $scope.relatedTitle = 'Intimate Partner Sexual Violence'
+    }
+    if ($scope.articleID == "Minnesota") {
+      //$scope.articleURL = 'https://cdn.rawgit.com/amgreenstreet/BeliefMeToo/master/public/articles/' + $routeParams.articleID + '.rtf';
       $scope.relatedURL = '/article/Drepression'
       $scope.relatedTitle = 'Intimate Partner Sexual Violence'
     }
